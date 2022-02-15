0be4db48f1f102e6343fd95ad119e547af7b60fa
Dmytro Zinchenko1 <dmytro_zinchenko1@epam.com>
2022-02-15 07:00:55 +0000

    [EDP-DDM-11688] Move all external mocks to the new helmfile

--- resources/repositories/cluster-mgmt.git/stages/Helmfile.groovy
+++ resources/repositories/cluster-mgmt.git/stages/Helmfile.groovy
@@ -5,8 +5,9 @@ import com.epam.edp.stages.impl.ci.Stage
 class Helmfile {
     Script script
 
+    ArrayList<String> COMPOSITE_COMPONENTS = ["user-management", "external-integration-mocks"]
+
     void run(context) {
-        def resultTag
         script.openshift.withCluster() {
             script.openshift.withProject() {
                 script.dir("${context.workDir}") {
@@ -14,14 +15,15 @@ class Helmfile {
                     script.env.ciProject = context.job.ciProject
                     script.env.dnsWildcard = context.job.dnsWildcard
                     script.env.edpName = context.job.edpName
-
-                    script.env.edpComponentDockerRegistryUrl = context.platform.getJsonPathValue("edpcomponent", "docker-registry", ".spec.url")
+                    script.env.edpComponentDockerRegistryUrl = context.platform.getJsonPathValue("edpcomponent",
+                            "docker-registry", ".spec.url")
                     // TODO: find more elegant way to distinct target env and our CICD env
-                    script.env.globalEDPProject = context.job.dnsWildcard.startsWith("apps.cicd") ? 'mdtu-ddm-edp-cicd' : context.job.edpName
+                    script.env.globalEDPProject = context.job.dnsWildcard.startsWith("apps.cicd") ?
+                            'mdtu-ddm-edp-cicd' : context.job.edpName
                     script.env.edpProject = context.job.ciProject
                     script.env.dockerRegistry = script.env.edpComponentDockerRegistryUrl
                     script.env.dockerProxyRegistry = script.env.edpComponentDockerRegistryUrl
-                    script.env.autoRedirectEnabled = context.job.dnsWildcard.startsWith("apps.cicd") ? 'true' : 'false'
+                    script.env.autoRedirectEnabled = context.job.dnsWildcard.startsWith("apps.cicd")
                     script.env.cloudProvider = script.sh(script: """oc get infrastructure cluster --no-headers -o jsonpath='{.status.platform}'""", returnStdout: true).trim()
                     script.env.backupBucket = script.sh(script: """ oc get secret -n ${script.env.edpProject} backup-credentials -o jsonpath='{.data.backup-s3-like-storage-location}' | base64 -d """, returnStdout: true).trim()
                     script.env.dockerhub_username = script.sh(script: """ oc get secret -n openshift-config pull-secret -o jsonpath='{.data.\\.dockerconfigjson}' | base64 -d | jq -r '.auths."https://index.docker.io/v2/".username' """, returnStdout: true).trim()
@@ -33,73 +35,40 @@ class Helmfile {
                     script.env.baseDomain = script.sh(script: """oc get dns cluster --no-headers -o jsonpath='{.spec.baseDomain}'""", returnStdout: true).trim()
                     script.env.idgovuaClientId = script.sh(script: """ oc get secret -n ${script.env.edpProject} id-gov-ua-client-secret -o jsonpath='{.data.clientId}' | base64 -d -w0 """, returnStdout: true).trim()
                     script.env.idgovuaClientSecret = script.sh(script: """ oc get secret -n ${script.env.edpProject} id-gov-ua-client-secret -o jsonpath='{.data.clientSecret}' | base64 -d -w0 """, returnStdout: true).trim()
-
                     script.env.globalNexusNamespace = context.job.dnsWildcard.startsWith("apps.cicd") ? 'mdtu-ddm-edp-cicd' : script.env.dockerRegistry.replaceAll(/.*\.(.*)\.svc:[0-9]+/, '\$1')
 
-                    def helmfile = 'deploy-templates/helmfile.yaml'
-                    def clustermgmt = 'properties/cluster-mgmt.yaml'
-                    def usermanagementHelmfile
-                    def components = script.readYaml file: clustermgmt
-                    def helmfileYaml = script.readYaml file: helmfile
-                    def gitURL = "ssh://${context.git.autouser}@${context.git.host}:${context.git.sshPort}/"
+                    String helmfile = 'deploy-templates/helmfile.yaml'
+                    String clustermgmt = 'properties/cluster-mgmt.yaml'
+                    String gitURL = "ssh://${context.git.autouser}@${context.git.host}:${context.git.sshPort}/"
+                    LinkedHashMap components = script.readYaml file: clustermgmt
+                    ArrayList<String> registries = script.sh(script: """oc get codebase -n ${script.env.globalEDPProject} -o=jsonpath='{.items[?(@.spec.type == "registry")].metadata.name}'""", returnStdout: true).trim().tokenize(" ")
 
-                    def registries = script.sh(script: """oc get codebase -n ${script.env.globalEDPProject} -o=jsonpath='{.items[?(@.spec.type == "registry")].metadata.name}'""", returnStdout: true).trim().tokenize(" ")
-
-                    components.releases.each {
-                        if (it.name == 'user-management') {
-                            usermanagementHelmfile = '/opt/repositories/' + it.labels.path + '/user-management.git/deploy-templates/helmfile.yaml'
-                        }
-                        if (it.labels.type == "remote") {
-                            script.dir('/opt/repositories/' + it.labels.path + '/' + it.name + '.git') {
-                                script.checkout([$class                           : 'GitSCM', branches: [[name: it.labels.branch]],
+                    components.releases.each { component ->
+                        if (component.labels.type == "remote") {
+                            script.dir('/opt/repositories/' + component.labels.path + '/' + component.name + '.git') {
+                                script.checkout([$class                           : 'GitSCM', branches: [[name: component.labels.branch]],
                                                  doGenerateSubmoduleConfigurations: false, extensions: [],
                                                  submoduleCfg                     : [],
                                                  userRemoteConfigs                : [[credentialsId: context.git.credentialsId,
-                                                                                      url          : gitURL + it.labels.path + it.name]]])
-                            }
-                        }
-                    }
-
-                    helmfileYaml.releases.each { release, releaseIndex ->
-                        if (release.labels.type == "remote") {
-                            script.dir('/opt/repositories/' + release.labels.path + '/' + release.name + '.git') {
-                                script.checkout([$class                           : 'GitSCM', branches: [[name: release.labels.branch]],
-                                                 doGenerateSubmoduleConfigurations: false, extensions: [],
-                                                 submoduleCfg                     : [],
-                                                 userRemoteConfigs                : [[credentialsId: context.git.credentialsId,
-                                                                                      url          : gitURL + release.labels.path + release.name]]])
+                                                                                      url          : gitURL + component.labels.path + component.name]]])
                             }
                         }
 
-                        // SCC
-                        if (release.labels.update_scc == true) {
-                            script.sh "oc create namespace ${release.namespace} --dry-run=true -o yaml | oc apply -f -"
-                            ['anyuid', 'privileged'].each { scc ->
-                                try {
-                                    context.platform.addSccToUser(release.name, scc, release.namespace)
+                        COMPOSITE_COMPONENTS.each { cc ->
+                            if (component.name == cc) {
+                                String ccHelmfile = "/opt/repositories/${component.labels.path}/${cc}.git/deploy-templates/helmfile.yaml"
+                                LinkedHashMap ccYaml = script.readYaml file: ccHelmfile
+                                ccYaml.releases.each { release ->
+                                    if (release.labels.type == "remote") {
+                                        script.dir('/opt/repositories/' + release.labels.path + '/' + release.name + '.git') {
+                                            script.checkout([$class                           : 'GitSCM', branches: [[name: release.labels.branch]],
+                                                             doGenerateSubmoduleConfigurations: false, extensions: [],
+                                                             submoduleCfg                     : [],
+                                                             userRemoteConfigs                : [[credentialsId: context.git.credentialsId,
+                                                                                                  url          : gitURL + release.labels.path + release.name]]])
+                                        }
+                                    }
                                 }
-                                catch (e) {
-                                    script.println e
-                                    def sleepSeconds = new Random().nextInt(60)
-                                    script.sh "sleep ${sleepSeconds}"
-                                    script.println "Trying to add SCC one more time"
-                                    context.platform.addSccToUser(release.name, scc, release.namespace)
-                                }
-
-                            }
-                            context.platform.createRoleBinding("system:serviceaccount:${release.name}", "view", release.namespace)
-                        }
-                    }
-
-                    def usermanagementYaml = script.readYaml file: usermanagementHelmfile
-                    usermanagementYaml.releases.each {
-                        if (it.labels.type == "remote") {
-                            script.dir('/opt/repositories/' + it.labels.path + '/' + it.name + '.git') {
-                                script.checkout([$class                           : 'GitSCM', branches: [[name: it.labels.branch]],
-                                                 doGenerateSubmoduleConfigurations: false, extensions: [],
-                                                 submoduleCfg                     : [],
-                                                 userRemoteConfigs                : [[credentialsId: context.git.credentialsId,
-                                                                                      url          : gitURL + it.labels.path + it.name]]])
                             }
                         }
                     }
@@ -112,16 +81,13 @@ class Helmfile {
                         script.sh("helmfile -f ${helmfile} -l name=ip-restrictions sync")
 
                         // add new branches from template to registry repo
-                        def templateURL
+                        String templateURL
                         script.sh("git config --global user.email \"you@example.com\"; git config --global user.name \"Admin\"")
                         registries.each { registry ->
                             templateURL = script.sh(script: "oc get codebase -n ${script.env.globalEDPProject} ${registry} -o jsonpath='{.spec.repository.url}'", returnStdout: true).trim().replaceAll(/https:\/\/.*?\\/(.*)/, "\$1")
                             script.dir(registry) {
-                                script.withCredentials([script.sshUserPrivateKey(credentialsId: "${context.git.credentialsId}",
-                                        keyFileVariable: 'key', passphraseVariable: '', usernameVariable: 'git_user')]) {
+                                context.script.sshagent(["${context.git.credentialsId}"]) {
                                     script.sh """
-                                        eval `ssh-agent`
-                                        ssh-add ${script.key}
                                         mkdir -p ~/.ssh
                                         ssh-keyscan -p ${context.git.sshPort} ${context.git.host} >> ~/.ssh/known_hosts
                                         git clone ${gitURL}${registry} target
@@ -146,12 +112,9 @@ class Helmfile {
                                     """
                                 }
                             }
-
                         }
-
                         script.sh("helmfile -f ${helmfile} sync --concurrency 1")
                     }
-
                 }
             }
         }
