2ab55c8acf1583882f9c266b52d15db3ff910b5e
Anatolii Stoliarov <anatolii_stoliarov@epam.com>
2022-07-12 13:55:48 +0300

    [EDP-DDM-13456] Add new cicd2 medium template with geo-server.

---
+++ resources/repositories/templates/registry-tenant-template-cicd2-geo-server-medium.git/scripts/delete_keycloakrealms_cr.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+export NAMESPACE=$1
+
+#if [[ ${NAMESPACE} == '' ]]; then
+#  echo "NAMESPACE is not defined, exiting"
+#  exit 1
+#fi
+#
+#
+#echo "Removing keycloakrealms in '${NAMESPACE}':"
+#while [[ `oc get keycloakrealms.v1.edp.epam.com -n ${NAMESPACE} --no-headers --output=custom-columns=NAME:.metadata.name` != '' ]]; do
+#  sleep 5
+#  oc get keycloakrealms.v1.edp.epam.com -n ${NAMESPACE} --no-headers --output=custom-columns=NAME:.metadata.name | xargs -r oc delete keycloakrealms.v1.edp.epam.com -n ${NAMESPACE}
+#done
+
